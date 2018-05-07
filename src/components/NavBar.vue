a07fe384ed3cdd78f6b59a0b06f4486f372a1ae0
Chinwei.Hsu <chinwei.hsu@kkday.com>
2018-05-07 10:15:03 +0800

    add flag icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,17 @@
 <template>
   <v-toolbar app>
     <img :src="logo" />
-    <v-btn >About</v-btn>
-    <v-btn>Roadmap</v-btn>
-    <v-btn>Token Details</v-btn>
-    <v-btn>Team</v-btn>
-    <v-btn>Contact</v-btn>
+    <v-btn flat>About</v-btn>
+    <v-btn flat>Roadmap</v-btn>
+    <v-btn flat>Token Details</v-btn>
+    <v-btn flat>Team</v-btn>
+    <v-btn flat>Contact</v-btn>
     <v-menu offset-y>
-      <v-btn color="primary" dark slot="activator">language</v-btn>
+      <v-btn flat color="primary" dark slot="activator">language</v-btn>
       <v-list>
         <v-list-tile>
           <v-list-tile-action>
+            <img class="flag" :src="UK"/>
           </v-list-tile-action>
               <v-list-tile-content>
                 <v-list-tile-title>English</v-list-tile-title>
@@ -18,6 +19,7 @@
         </v-list-tile>
         <v-list-tile>
           <v-list-tile-action>
+            <img class="flag" :src="TW"/>
           </v-list-tile-action>
               <v-list-tile-content>
                 <v-list-tile-title>中文</v-list-tile-title>
@@ -29,18 +31,30 @@
     <v-btn icon large><i class="fab fa-telegram-plane"></i></v-btn>
     <v-btn icon large><i class="fab fa-twitter"></i></v-btn>
     <v-btn icon large><i class="fab fa-medium-m"></i></v-btn>
-    <v-btn>Contribute</v-btn>
+    <v-btn flat>Contribute</v-btn>
   </v-toolbar>
 </template>
 <script>
 import Logo from '@/assets/lecarbone_logo.png';
+import UK from '@/assets/flag_uk.png';
+import TW from '@/assets/flag_tw.png';
 
 export default {
   name: 'nav-bar',
   data() {
     return {
       logo: Logo,
+      UK,
+      TW,
     };
   },
 };
 </script>
+
+<style>
+.flag {
+  width: 30px;
+  height: 30px;
+}
+</style>
+
