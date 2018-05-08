30818cc1eddd369c7b65232968216555bc3da62d
Chinwei.Hsu <chinwei.hsu@kkday.com>
2018-05-08 17:09:20 +0800

    remove test page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/containers/HelloWorld';
 import HomePage from '@/containers/HomePage';
 import TokenSale from '@/containers/TokenSale';
 
@@ -10,11 +9,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-    {
-      path: '/homepage',
       name: 'HomePage',
       component: HomePage,
     },
