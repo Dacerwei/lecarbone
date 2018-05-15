fae9acfb6af240edbae3924c6ae4762bfbee367a
Chinwei.Hsu <chinwei.hsu@kkday.com>
2018-05-15 21:54:17 +0800

    add i18n packages

---
+++ src/i18n/en.js
@@ -0,0 +1,13 @@
+module.exports = {
+  message: {
+    NavBar: {
+      about: 'about',
+      roadMap: 'roadmap',
+      tokenDetail: 'token detail',
+      team: 'team',
+      contact: 'contact',
+      language: 'languages',
+      contribute: 'Contribute',
+    },
+  },
+};
---
+++ src/i18n/zh.js
@@ -0,0 +1,13 @@
+module.exports = {
+  message: {
+    NavBar: {
+      about: '關於',
+      roadMap: '計畫',
+      tokenDetail: '代幣細節',
+      team: '團隊',
+      contact: '聯絡我們',
+      language: '切換語言',
+      contribute: '貢獻',
+    },
+  },
+};
