e27976988527fc6dc37d3f628f54b3c4f9641ff6
Chinwei.Hsu <chinwei.hsu@kkday.com>
2018-05-22 21:32:40 +0800

    add progress bar

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,22 +1,59 @@
 <template>
-  <v-container>
-    <h1>
-      A cryptocurrency issued base on the vision of creating awareness
-      for the need of carbon offsets and corporate social responsibilities(CSR)
-    </h1>
-    <count-down-timer :end-date="'20180628'">
-    </count-down-timer>
-  </v-container>
+  <div class="main-section" :style="containerStyle">
+    <v-container>
+      <h1 class="main-text">
+        A cryptocurrency issued base on the vision of creating awareness
+        for the need of carbon offsets and corporate social responsibilities(CSR)
+      </h1>
+      <count-down-timer :end-date="endDate"></count-down-timer>
+      <prgress-bar :total="total" :current-value="progress"></prgress-bar>
+      <v-layout row>
+        <v-btn outline color="green">Contribute Now</v-btn>
+        <div>
+          <p>White Paper Download</p>
+          <v-layout row>
+            <v-btn outline color="white">ENGLISH</v-btn>
+            <v-btn outline color="white">中文</v-btn>
+          </v-layout>
+        </div>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import CountDownTimer from '@/components/CountDownTimer';
+import PrgressBar from '@/components/ProgressBar';
+import MainSectionBackground from '@/assets/main_section_background.jpg';
 
 export default {
   name: 'MainSection',
+  data() {
+    return {
+      endDate: '20180628 1200',
+      progress: 200,
+      total: 1000,
+      containerStyle: {
+        backgroundImage: `url(${MainSectionBackground})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      },
+    };
+  },
   components: {
     CountDownTimer,
+    PrgressBar,
+  },
+  methods: {
   },
 };
 </script>
+
+<style scope>
+.main-text {
+  color: white;
+  text-align: left;
+  font-weight: bold;
+}
+</style>
 
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-layout row justify-space-between>
+      <span class="label">0</span>
+      <span class="label">{{ total/2 }}</span>
+      <span class="label">{{ total }}</span>
+    </v-layout>
+    <v-progress-linear
+      :value="progress"
+      height="30"
+      color="green"
+      style="border-radius: 10px">
+    </v-progress-linear>
+    <v-layout row justify-space-between>
+      <span class="label">START</span>
+      <span class="label">HARD CAP</span>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ProgressBar',
+  props: {
+    total: {
+      type: Number,
+      default: 100,
+    },
+    currentValue: {
+      type: Number,
+      default: 0,
+    },
+  },
+  updated() {
+    // eslint-disable-next-line
+    console.log(this.progress);
+  },
+  computed: {
+    progress() {
+      return 100 * (this.currentValue / this.total);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.label {
+  color: white;
+}
+</style>
+
