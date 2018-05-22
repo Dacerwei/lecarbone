90e90af8c02d00e9f7593ba9d5992ac6cde79243
Chinwei.Hsu <chinwei.hsu@kkday.com>
2018-05-23 03:29:38 +0800

    timer style update

--- src/components/CountDownTimer.vue
+++ src/components/CountDownTimer.vue
@@ -1,16 +1,40 @@
 <template>
-  <v-container>
+  <v-flex tag="div" xs12 sm6 md4>
     <div class="timer-title">Main Sale Started!</div>
     <div class="timer-text">till {{ dueDate.format('MMMM Do, h:mm a') }} PDT</div>
-    <v-layout row>
-      <span>{{ year }} years</span>
-      <span>{{ month }} months</span>
-      <span>{{ day }} days</span>
-      <span>{{ hour }} hours</span>
-      <span>{{ minute }} minutes</span>
-      <span>{{ second }} seconds</span>
+    <v-layout row wrap justify-space-between >
+      <v-flex tag='li' sm1>
+        <div class="timer-item">
+          <div class="timer-time">{{ month }}</div>
+          <div class="timer-label">MONTHS</div>
+        </div>
+      </v-flex>
+      <v-flex tag='li' sm1>
+        <div class="timer-item">
+          <div class="timer-time">{{ day }}</div>
+          <div class="timer-label">DAYS</div>
+        </div>
+      </v-flex>
+      <v-flex tag='li' sm1>
+        <div class="timer-item">
+          <div class="timer-time">{{ hour }}</div>
+          <div class="timer-label">HOURS</div>
+        </div>
+      </v-flex>
+      <v-flex tag='li' sm1>
+        <div class="timer-item">
+          <div class="timer-time">{{ minute }}</div>
+          <div class="timer-label">MINUTES</div>
+        </div>
+      </v-flex>
+      <v-flex tag='li' sm1>
+        <div class="timer-item">
+          <div class="timer-time">{{ second }}</div>
+          <div class="timer-label">SECONDS</div>
+        </div>
+      </v-flex>
     </v-layout>
-  </v-container>
+  </v-flex>
 </template>
 
 <script>
@@ -70,13 +94,34 @@ export default {
 };
 </script>
 <style scoped>
+.timer-wrapper {
+  width: 50%;
+  margin-left: 0;
+  min-width: 300px;
+}
 .timer-title {
-  font-size: 24pt;
+  font-size: 200%;
   text-align: left;
   color: yellow;
 }
 .timer-text {
   color: yellow;
+}
+.timer-item {
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  width: 60px;
+  border-radius: 10px;
+}
+.timer-time {
+  font-size: 150%;
+  padding: 2px;
+}
+.timer-label {
+  font-size: 50%;
+  color: grey;
+  font-weight: bold;
 }
 </style>
 
