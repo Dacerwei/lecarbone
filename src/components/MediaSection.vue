b215eac61fc5f2df799a3bc5661b1bff2a4b275f
Chinwei.Hsu <chinwei.hsu@kkday.com>
2018-05-24 02:51:45 +0800

    media section style and icons

--- src/components/MediaSection.vue
+++ src/components/MediaSection.vue
@@ -1,19 +1,20 @@
 <template>
+<div>
+  <v-layout row wrap align-content-center justify-space-between class="social-media-wrapper">
+    <v-flex xs6 sm2 v-for="media in socialMediaList" :key="media.id">
+      <img class="media-item"
+        :src="media.src"
+        alt=""/>
+    </v-flex>
+  </v-layout>
   <v-container>
-    <v-layout row wrap>
-      <v-flex xs6 sm3 v-for="media in socialMediaList" :key="media.id">
-        <img class="media-item"
-          :src="media.src"
-          alt=""/>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap>
+    <v-layout row wrap justify-space-between>
       <v-flex xs4 sm3 v-for=" media in mediaList" :key="media.id">
-        <h3>{{ media.name }}</h3>
-        <img :src="media.src" :alt="media.name">
+        <div :style="formatStyle(media)"></div>
       </v-flex>
     </v-layout>
   </v-container>
+</div>
 </template>
 
 <script>
@@ -21,6 +22,18 @@ import Facebook from '@/assets/media/facebook.png';
 import Twitter from '@/assets/media/twitter.png';
 import Medium from '@/assets/media/medium.png';
 import Telegram from '@/assets/media/telegram.png';
+import bbc from '@/assets/media/bbc.png';
+import bloomberg from '@/assets/media/bloomberg.png';
+import controlledEnvironments from '@/assets/media/controlled_environments.png';
+import cnn from '@/assets/media/cnn.png';
+import ecolog from '@/assets/media/ecolog.png';
+import forbes from '@/assets/media/forbes.png';
+import newYorTimes from '@/assets/media/new_york_times.png';
+import techcrunch from '@/assets/media/techcrunch.png';
+import theWallStreetJournal from '@/assets/media/the_wall_street_journal.png';
+import theWashingtonPost from '@/assets/media/the_washington_post.png';
+import time from '@/assets/media/time.png';
+import greenpaece from '@/assets/media/greenpaece.png';
 
 const socialMediaList = [
   {
@@ -49,62 +62,62 @@ const mediaList = [
   {
     id: 0,
     name: 'the wall street journal',
-    src: '',
+    src: theWallStreetJournal,
   },
   {
     id: 1,
     name: 'the washington post',
-    src: '',
+    src: theWashingtonPost,
   },
   {
     id: 2,
     name: 'time',
-    src: '',
+    src: time,
   },
   {
     id: 3,
     name: 'bloomberg',
-    src: '',
+    src: bloomberg,
   },
   {
     id: 4,
     name: 'cnn',
-    src: '',
+    src: cnn,
   },
   {
     id: 5,
     name: 'bbc',
-    src: '',
+    src: bbc,
   },
   {
     id: 6,
     name: 'controlled environments',
-    src: '',
+    src: controlledEnvironments,
   },
   {
     id: 7,
     name: 'the new york times',
-    src: '',
+    src: newYorTimes,
   },
   {
     id: 8,
-    name: 'greenpeace',
-    src: '',
+    name: 'greenpaece',
+    src: greenpaece,
   },
   {
     id: 9,
     name: 'forbes',
-    src: '',
+    src: forbes,
   },
   {
     id: 10,
     name: 'techcrunch',
-    src: '',
+    src: techcrunch,
   },
   {
     id: 11,
     name: 'ecolog',
-    src: '',
+    src: ecolog,
   },
 ];
 
@@ -116,12 +129,31 @@ export default {
       socialMediaList,
     };
   },
+  methods: {
+    formatStyle(obj) {
+      return {
+        height: '50px',
+        margin: '10%',
+        backgroundImage: `url(${obj.src})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'contain',
+      };
+    },
+  },
 };
 </script>
 
 <style scoped>
+.social-media-wrapper {
+  background-color: white;
+  width: 100%;
+  padding-top: 20px;
+  padding-left: 10%;
+  padding-right: 10%;
+}
 .media-item {
   width: 100%;
+  margin: 5%;
 }
 </style>
 
