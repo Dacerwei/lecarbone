65ea9cbc0782cbc0f015b88ae38e6402ffba5d42
Chinwei.Hsu <chinwei.hsu@kkday.com>
2018-06-03 23:09:44 +0800

    add affiliate section

---
+++ src/components/AffiliateSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm4>
+        <h1>Affiliate Program</h1>
+      </v-flex>
+      <v-flex xs12 sm8>
+        <p>
+          Additional 0.5% bonus token you will get from all your
+          friends that will use your referral link!
+          Any user registered with this link will get 0.2 LeCarbone token as a welcome bonus.
+        </p>
+        <div>
+          Your Referral Link: <span>https://tokensale.lecarbone.com/r/34532</span><span><v-btn outline color="green">COPY</v-btn></span>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'AffiliateSection',
+};
+</script>
