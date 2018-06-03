eebb496ac9eaac0753bb0a4ccf4f50b21ef7282b
Chinwei.Hsu <chinwei.hsu@kkday.com>
2018-06-04 02:39:39 +0800

    adjusted q&a section

--- src/components/QuestionsSection.vue
+++ src/components/QuestionsSection.vue
@@ -1,11 +1,15 @@
 <template>
   <v-container>
-    <h1>FAQ</h1>
-    <ul>
-      <li v-for="q in questions" :key="q.id">
-        <h2>{{ q.questions }}</h2>
-        <p>{{ q.answer }}</p>
-      </li>
+    <h1 class="qa-title">FAQ</h1>
+    <hr class="title-divider">
+    <ul class="qa-list">
+      <template v-for="q in questions">
+        <li class="qa-list-item" :key="q.id">
+          <h2>{{ q.questions }}</h2>
+          <p>{{ q.answer }}</p>
+        </li>
+        <hr :key="q.id + 'divider'">
+      </template>
     </ul>
   </v-container>
 </template>
@@ -41,3 +45,27 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.qa-title {
+  text-align: center;
+  color: #F1CF69;
+  margin: 5px;
+}
+.title-divider {
+  margin: 0 auto;
+  width: 5%;
+  border: 0;
+  background-color: #F1CF69;
+  height: 2px;
+}
+.qa-list {
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+.qa-list-item {
+  font-weight: bold;
+  list-style: none;
+  padding: 10px;
+}
+</style>
