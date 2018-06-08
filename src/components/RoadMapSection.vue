4cb54677ddc4731049ce555f71ef2874b481be22
Yan-Boogie <ian24257587@gmail.com>
2018-06-08 11:22:23 +0800

    unstage eslintrc.js&stage roadmapSection

--- src/components/RoadMapSection.vue
+++ src/components/RoadMapSection.vue
@@ -2,7 +2,7 @@
     <section id="roadmap">
       <h1>Roadmap</h1>
       <hr class="title" />
-      <div class="roadmap">
+      <div class="roadmap" v-if = "!isMobile">
         <div class="uppermap">
           <div class="up_container">
             <div class="utitle">2017 Q1</div>
@@ -94,24 +94,110 @@
           </div>
         </div>
       </div>
+      <div class="roadmapMobile" v-if = "isMobile">
+        <div class="qContainer" v-for = "question in questionList" :key="question.id">
+          <div class="point"></div>
+          <h2 class="qTitle">{{question.title}}</h2>
+          <p class="qDescribtion" v-html="question.describtion"></p>
+        </div>
+      </div>
     </section>
 </template>
 
 <script>
+const questionList = [
+  {
+    id: 0,
+    title: 'Q1 2017',
+    describtion: 'Indonesian Chamber of Commerce and Industry enter into contract with Pelestarian Alam Nusantara Foundation Forest protection & poverty alleviation education',
+  },
+  {
+    id: 1,
+    title: 'Q2 2017',
+    describtion: 'Forest protection & poverty alleviation education',
+  },
+  {
+    id: 2,
+    title: 'Q3 2017',
+    describtion: 'Land acquisition and felling permits from the Government.<br/>Land development.',
+  },
+  {
+    id: 3,
+    title: 'Q1 2017',
+    describtion: 'Legel consultancy Under-forest operation Private Sale',
+  },
+  {
+    id: 4,
+    title: 'Q2 2018',
+    describtion: 'Baseline survey Poverty alleviation material transpotation & allocation',
+  },
+  {
+    id: 5,
+    title: 'Q3 2018',
+    describtion: 'Agarwood formation and fungus-growing',
+  },
+  {
+    id: 6,
+    title: 'Q4 2018',
+    describtion: 'Certification of carbon credits completed. Price of carbon credits set, with announcement on amount carbon credits acquired',
+  },
+  {
+    id: 7,
+    title: 'Q1 2019',
+    describtion: 'Launch of online exchange for carbon credits.',
+  },
+  {
+    id: 8,
+    title: 'Q2 2019',
+    describtion: 'Launch of online charity donation system. Launch of online exchange system for sponsored goods.',
+  },
+  {
+    id: 9,
+    title: 'Q3 2019',
+    describtion: 'Launch of online exchange system for under-forest products.',
+  },
+  {
+    id: 10,
+    title: 'Q4 2019',
+    describtion: 'Contract renewal',
+  },
+];
+
 export default {
   name: 'RoadMap',
+  data() {
+    return {
+      isMobile: false,
+      questionList,
+    };
+  },
+  mounted() {
+    this.onResize();
+    window.addEventListener('resize', this.onResize, { passive: true });
+  },
+  beforeDestroy() {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.onResize, { passive: true });
+    }
+  },
+  methods: {
+    onResize() {
+      this.isMobile = window.innerWidth < 600;
+      // this.isMobile = window.innerWidth < 600
+    },
+  },
 };
 </script>
 
 <style lang="sass" scoped>
 $color_yellow: #F1CF69
 *
-  // border: solid 1px
   position: relative
+  // border: solid 1px
   // font-family:
 
 #roadmap
-  padding: 30px 0;
+  padding: 30px 0
   h1
     text-align: center
     margin-bottom: 5px
@@ -121,7 +207,7 @@ $color_yellow: #F1CF69
     width: 40px
     border: 1px solid $color_yellow
     background-color: $color_yellow
-    margin: 0 auto 40px auto;
+    margin: 0 auto 40px auto
   .uppermap
     font-size: 0px
     padding-left: 4%
@@ -206,4 +292,26 @@ $color_yellow: #F1CF69
     width: 100%
     margin: 0px
     border: solid 1px $color_yellow
+  .roadmapMobile
+    padding: 0% 8% 0% 20%
+    .qContainer
+      padding: 0px 20px
+      height: 250px
+      border-left: solid 1px $color_yellow
+      .point
+        position: absolute
+        width: 15px
+        height: 15px
+        border-radius: 50%
+        top: 0px
+        left: 0px
+        transform: translate(-50%, -50%)
+        background-color: $color_yellow
+      .qTitle
+        // margin-top: -20px
+        bottom: 20px
+      .qDescribtion
+        margin-top: -15px
+
+
 </style>
